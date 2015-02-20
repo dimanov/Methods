7a5ea7486e517fa5eebecb9dd14c67b0ad111198
dimanov <dimanov@gmail.com>
2015-02-20 22:05:55 +0200

    First problem added.

---
+++ 01. SayHello/SayHello.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Problem 1. Say Hello
+ * Write a method that asks the user for his name and prints �Hello, <name>�
+ * Write a program to test this method.
+ */
+
+using System;
+
+class SayHello
+{
+    static void Main()
+    {
+        Console.WriteLine("What is you name?");
+        string name = Console.ReadLine();
+
+        SayHallo(name);
+
+    }
+
+    private static void SayHallo(string name)
+    {
+        Console.WriteLine("Hello, {0}!", name);
+    }
+}
